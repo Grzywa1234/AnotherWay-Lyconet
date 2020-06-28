8e73e63e1444a84a5cd4a927564a935c4f49c300
Grzywa1234 <devillkazuya@gmail.com>
2020-06-28 22:23:55 +0200

    working homepage

---
+++ script.js
@@ -0,0 +1,115 @@
+/*
+navbar span styling
+ */
+const menuBtn = document.querySelector('.menuBtn1');
+const menuBtn2 = document.querySelector('.menuBtn2');
+const menuBtn3 = document.querySelector('.menuBtn3');
+const arrone = document.querySelector('.arrone');
+const arrtwo = document.querySelector('.arrtwo');
+const arrthree = document.querySelector('.arrthree');
+
+
+menuBtn.addEventListener('mouseover', opacityArrOne);
+menuBtn.addEventListener('mouseout', opacityOneZero);
+menuBtn2.addEventListener('mouseover', opacityArrTwo);
+menuBtn2.addEventListener('mouseout', opacityTwoZero);
+menuBtn3.addEventListener('mouseover', opacityArrThree);
+menuBtn3.addEventListener('mouseout', opacityThreeZero);
+
+
+
+function opacityArrOne() {
+    arrone.style.opacity = '1';
+}
+
+function opacityArrTwo() {
+    arrtwo.style.opacity = '1';
+}
+
+function opacityArrThree() {
+    arrthree.style.opacity = '1';
+}
+
+function opacityOneZero(){
+    arrone.style.opacity = '0';
+}
+
+function opacityTwoZero(){
+    arrtwo.style.opacity = '0';
+}
+
+function opacityThreeZero(){
+    arrthree.style.opacity = '0';
+}
+
+
+/*
+menu
+*/
+const showMenuBtn = document.querySelector('.fa-bars');
+const hideMenuBtn  = document.querySelector('.fa-times-circle');
+const menu = document.querySelector('.menu');
+
+function show() {
+    menu.style.transform = "translateX(100%)"
+}
+
+function hide() {
+    menu.style.transform = "translateX(0)";
+}
+
+
+menuBtn.addEventListener('click', hide);
+menuBtn2.addEventListener('click', hide);
+menuBtn3.addEventListener('click', hide);
+hideMenuBtn.addEventListener('click', hide);
+showMenuBtn.addEventListener('click', show);
+/*
+wrap 
+ */
+
+let sliderImages = document.querySelectorAll('.slide'),
+    arrowLeft = document.querySelector('#arrow-left'),
+    arrowRight = document.querySelector('#arrow-right'),
+    current = 0;
+
+
+function reset(){
+    for(let i = 0; i < sliderImages.length; i++){
+        sliderImages[i].style.display = 'none';
+    }
+}
+
+function startSlide(){
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+
+function slideLeft(){
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+
+function slideRight(){
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+
+arrowLeft.addEventListener('click', function(){
+    if (current === 0){
+        current = sliderImages.length
+    }
+    slideLeft();
+})
+
+arrowRight.addEventListener('click', function(){
+    if (current === sliderImages.length - 1){
+        current = -1;
+    }
+    slideRight();
+})
+
+
+startSlide();
