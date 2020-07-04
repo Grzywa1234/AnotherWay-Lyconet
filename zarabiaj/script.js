da05b2cbb1b181aa9db7822922562a7e64a4a634
Grzywa1234 <devillkazuya@gmail.com>
2020-07-04 23:16:23 +0200

    working on menu

--- zarabiaj/script.js
+++ zarabiaj/script.js
@@ -1,5 +1,5 @@
 /*
-navbar span styling
+navbar span & sub menu styling
  */
 const menuBtn = document.querySelector('.menuBtn1');
 const menuBtn2 = document.querySelector('.menuBtn2');
@@ -12,6 +12,9 @@ const arrthree = document.querySelector('.arrthree');
 const arrfour = document.querySelector('.arrfour');
 const arrfive = document.querySelector('.arrfive');
 
+const subOne = document.querySelector('.sub1');
+const subTwo = document.querySelector('.sub2');
+const subThree = document.querySelector('.sub3');
 
 menuBtn.addEventListener('mouseover', opacityArrOne);
 menuBtn.addEventListener('mouseout', opacityOneZero);
@@ -24,18 +27,26 @@ menuBtn4.addEventListener('mouseout', opacityFourZero);
 menuBtn5.addEventListener('mouseover', opacityArrFive);
 menuBtn5.addEventListener('mouseout', opacityFiveZero);
 
-
+subOne.addEventListener('mouseover', opacityArrOne);
+subOne.addEventListener('mouseout', opacityOneZero);
+subTwo.addEventListener('mouseover', opacityArrTwo);
+subTwo.addEventListener('mouseout', opacityTwoZero);
+subThree.addEventListener('mouseover', opacityArrThree);
+subThree.addEventListener('mouseout', opacityThreeZero);
 
 function opacityArrOne() {
     arrone.style.opacity = '1';
+    subOne.style.display = 'block';
 }
 
 function opacityArrTwo() {
     arrtwo.style.opacity = '1';
+    subTwo.style.display = 'block';
 }
 
 function opacityArrThree() {
     arrthree.style.opacity = '1';
+    subThree.style.display = 'block';
 }
 
 function opacityArrFour() {
@@ -45,16 +56,20 @@ function opacityArrFour() {
 function opacityArrFive() {
     arrfive.style.opacity = '1';
 }
+
 function opacityOneZero(){
     arrone.style.opacity = '0';
+    subOne.style.display = 'none';
 }
 
 function opacityTwoZero(){
     arrtwo.style.opacity = '0';
+    subTwo.style.display = 'none';
 }
 
 function opacityThreeZero(){
     arrthree.style.opacity = '0';
+    subThree.style.display = 'none';
 }
 
 function opacityFourZero(){
